9904dfac2f979b2e0f3d0ac3a2b46afde0e229e8
stemtazoo <43175204+stemtazoo@users.noreply.github.com>
2024-12-11 16:26:10 +0900

    third commit

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup, find_packages
 
+# Load the version from the package
+with open("streamlit_data_viz_helper/__init__.py") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            __version__ = line.split("=")[1].strip().strip("\"'")
+            break
+
 setup(
-    name="streamlit_data_viz_helper",
-    version="0.1.1",
+    name="streamlit-data-viz-helper",
+    version=__version__,  # Use the version from __init__.py
+    description="Tools and utilities for building data visualization apps with Streamlit.",
+    author="Your Name",
     packages=find_packages(),
     install_requires=[
-        "pandas",
-        "chardet",
         "streamlit",
-        "plotly"
+        "pandas",
+        "plotly",
+        "chardet"
     ],
-    description="Tools and utilities for building data visualization apps with Streamlit.",
-    author="stemtazoo",
-    author_email="stem.sci.tech.eng.math.2013@gmail.com",
 )
